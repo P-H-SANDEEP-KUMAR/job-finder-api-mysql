91c1647030b21d4a95eb325933e2f4ea141fb8bc
Sandeep Goud <sandeep@gmail.com>
2026-09-21 12:17:09 +0530

    Improve job search and database handling

--- job_finder.py
+++ job_finder.py
@@ -23,46 +23,53 @@ def save_jobs_to_mysql(jobs):
     connection = get_db_connection()
     cursor = connection.cursor()
 
-    query = """
-    INSERT IGNORE INTO jobs
-    (title, company_name, location, link, description)
-    VALUES (%s, %s, %s, %s, %s)
-"""
-    new_jobs = 0
-    for job in jobs:
-        cursor.execute(query, (
-            job["Title"],
-            job["Company Name"],
-            job["Location"],
-            job["Link"],
-            job["Description"]
-        ))
+    try:
+        query = """
+        INSERT IGNORE INTO jobs
+        (title, company_name, location, link, description)
+        VALUES (%s, %s, %s, %s, %s)
+        """
 
-        if cursor.rowcount == 1:
+        new_jobs = 0
+
+        for job in jobs:
+            cursor.execute(query, (
+                job["Title"],
+                job["Company Name"],
+                job["Location"],
+                job["Link"],
+                job["Description"]
+            ))
+
+            if cursor.rowcount == 1:
                 new_jobs += 1
 
-    connection.commit()
-    cursor.close()
-    connection.close()
+        connection.commit()
+
+    finally:
+        cursor.close()
+        connection.close()
 
     skipped_jobs = len(jobs) - new_jobs
 
     print(f"{new_jobs} new jobs inserted into MySQL.")
     print(f"{skipped_jobs} existing jobs skipped.")
-def search_jobs_from_mysql(job_keyword, location_keyword):
+def search_jobs_from_mysql(job_keyword, location_keyword,result_limit):
     connection = get_db_connection()
     cursor = connection.cursor(dictionary=True)
 
     query = """
-        SELECT title, company_name, location, link
+        SELECT title, company_name, location, link, description
         FROM jobs
         WHERE title LIKE %s
         AND location LIKE %s
+        Limit %s
     """
 
     cursor.execute(query, (
         f"%{job_keyword}%",
-        f"%{location_keyword}%"
+        f"%{location_keyword}%",
+        result_limit
     ))
 
     jobs = cursor.fetchall()
@@ -80,6 +87,12 @@ def display_mysql_jobs(jobs):
         print("\nTitle:", job["title"])
         print("Company:", job["company_name"])
         print("Location:", job["location"])
+        description = job["description"]
+
+        if len(description) > 200:
+            description = description[:200] + "..."
+
+        print("Description:", description)
         print("Link:", job["link"])
         print("--------------------")
 def clean_description(html_description):
@@ -112,45 +125,6 @@ def remove_duplicates(jobs):
     return unique_jobs
 
 
-def filter_jobs(jobs, job_keyword, location_keyword):
-    job_keyword = job_keyword.lower()
-    location_keyword = location_keyword.lower()
-    filtered_jobs = []
-    for job in jobs:
-        title_match = job_keyword in job["Title"].lower()
-        location_match = location_keyword in job["Location"].lower()
-        if title_match and location_match:
-            filtered_jobs.append(job)
-    return filtered_jobs
-
-
-def save_jobs(filtered_jobs):
-    with open("jobs.txt", "w", encoding="utf-8") as file:
-        for job in filtered_jobs:
-            text = f"""Title: {job["Title"]}
-Company: {job["Company Name"]}
-Location: {job["Location"]}
-Link: {job["Link"]}
-Description: {job["Description"]}
---------------------
-"""
-            file.write(text)
-
-
-def save_jobs_json(filtered_jobs):
-    with open("jobs.json", "w", encoding="utf-8") as file:
-        json.dump(filtered_jobs, file, indent=2, ensure_ascii=False)
-
-
-def display_jobs(filtered_jobs):
-    for job in filtered_jobs:
-        print(f"""
-Title: {job["Title"]}
-Company: {job["Company Name"]}
-Location: {job["Location"]}
-Link: {job["Link"]}
---------------------
-""")
 
 
 def main():
@@ -160,7 +134,16 @@ def main():
 
         job_keyword = input('enter job_keyword to search: ').strip()
         location_keyword = input('enter location_keyword to search: ').strip()
+        try:
+            result_limit = int(input("How many jobs do you want to display? "))
 
+            if result_limit <= 0:
+                print("Error: result limit must be greater than 0.")
+                return
+
+        except ValueError:
+            print("Error: please enter a valid number.")
+            return
         if not job_keyword or not location_keyword:
             print("Error: job_keyword and location_keyword cannot be empty.")
             return
@@ -211,7 +194,7 @@ def main():
         unique_jobs = remove_duplicates(clean_jobs)
         print("Unique jobs:", len(unique_jobs))
         save_jobs_to_mysql(unique_jobs)
-        mysql_jobs = search_jobs_from_mysql(job_keyword, location_keyword)
+        mysql_jobs = search_jobs_from_mysql(job_keyword, location_keyword,result_limit)
 
         print(f"MySQL matching jobs: {len(mysql_jobs)}")
         display_mysql_jobs(mysql_jobs)
